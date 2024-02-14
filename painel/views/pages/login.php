a4270cf985d7117ea09dceb1cdb5c1d439a0bc0c
matheus-9898 <98matheusm@gmail.com>
2024-02-14 10:08:49 -0300

    edit page login

--- painel/views/pages/login.php
+++ painel/views/pages/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login | Controle de estoque</title>
+    <title>Login</title>
     <link rel="stylesheet" href="views/css/login.css">
 </head>
 <body>
