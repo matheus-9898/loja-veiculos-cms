cc35c44e5250f0ad5c390b2677de36e6859d7406
matheus-9898 <98matheusm@gmail.com>
2024-02-15 19:52:25 -0300

    página mensagens

--- models/contatoModel.class.php
+++ models/contatoModel.class.php
@@ -1,8 +1,8 @@
 <?php 
     class contatoModel{
         public static function inputMensagem(){
-            $sql = mysqlModel::conexaoBD()->prepare('INSERT INTO mensagens VALUES (?,?,?,?,?,?)');
-            $sql->execute(array(null,$_POST['nome'],$_POST['email'],$_POST['telefone'],$_POST['mensagem'],0));
+            $sql = mysqlModel::conexaoBD()->prepare('INSERT INTO mensagens VALUES (?,?,?,?,?)');
+            $sql->execute(array(null,$_POST['nome'],$_POST['email'],$_POST['telefone'],$_POST['mensagem']));
             header('Location: '.ROOT_PATH.'contato');
             die();
         }
