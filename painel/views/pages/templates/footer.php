59a3112fba6c0c491b479fae2da5a9a0014ddbe4
matheus-9898 <98matheusm@gmail.com>
2024-02-18 22:20:54 -0300

    sem máscara

--- painel/views/pages/templates/footer.php
+++ painel/views/pages/templates/footer.php
@@ -2,11 +2,5 @@
     </main>
     <script src="views/js/jquery.js"></script>
     <script src="views/js/notify.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-    <script>
-        $(function(){
-            $('.contAddVeic input[name=valor]').mask('0.000.000,00',{reverse:true});
-        })
-    </script>
 </body>
 </html>
